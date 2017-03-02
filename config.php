6d97e06c04bf04b65f6e08af81de282e5a66344e
willoleary6 <15155528@studentmail.ul.ie>
2017-03-02 16:58:13 +0000

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('hostname', 'localhost');
-define('user', 'id922837_peoplesfrontofjudea245');
-define('password', 'AaronIsASillyBager');
-define('db_name', 'id922837_websiteuserdetails');
-?>
