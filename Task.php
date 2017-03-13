aee94a7ce0d3a31474e28836962251d04a2dfd65
ChunkyMitts <15160009@studentmail.ul.ie>
2017-03-13 12:11:31 +0000

    changes to create task and added php for create task

---
+++ Task.php
@@ -0,0 +1,15 @@
+<?php
+	include 'dbh.php';
+	$TaskTitle = 	$_POST['TaskTitle'];
+	$NumOfPages = 	$_POST['NumOfPages'];
+	$NumOfWords = 	$_POST['NumOfWords'];
+	$Tags1 = 		$_POST['Tags1'];
+	$Tags2 = 		$_POST['Tags2'];
+	$Tags3 = 		$_POST['Tags3'];
+	$Tags4 = 		$_POST['Tags4'];
+	$Description = 	$POST['Description'];
+	$ClaimBy = 		$POST['ClaimBy'];
+	$Completion =	$POST['Completion'];
+	$FileType = 	$POST['FileType'];
+	$SampleFile = 	$POST['SampleFile'];
+?>
