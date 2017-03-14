6ff736ecfc3544ea8e35558a5a60893eb5846964
ChunkyMitts <15160009@studentmail.ul.ie>
2017-03-14 17:02:38 +0000

    php modification
    cant get the tags to work yet with the php

--- Task.php
+++ Task.php
@@ -4,10 +4,15 @@
 	$NumOfPages = 	$_POST['NumOfPages'];
 	$TaskType =		$_POST['TaskType'];
 	$NumOfWords = 	$_POST['NumOfWords'];
-	//$Tags1 = 		$_POST['Tags1'];
-	//$Tags2 = 		$_POST['Tags2'];
-	//$Tags3 = 		$_POST['Tags3'];
-	//$Tags4 = 		$_POST['Tags4'];
+	$tags = 		$_POST['Tags'];
+	if($tags >= 1)
+		$Tags1 = 		$_POST['Tags1'];
+	if($tags >= 2)
+		$Tags2 = 		$_POST['Tags2'];
+	if($tags >= 3)
+		$Tags3 = 		$_POST['Tags3'];
+	if($tags == 4)
+		$Tags4 = 		$_POST['Tags4'];
 	$Description = 	$_POST['Description'];
 	$ClaimBy = 		$_POST['ClaimBy'];
 	$Completion =	$_POST['Completion'];
@@ -15,5 +20,18 @@
 	$SampleFile = 	$_POST['SampleFile'];
 	$sql = "INSERT INTO tasks(Title, Text_description, Task_type, Attached_Files, NoOfPages, NoOfWords, Deadline, ClaimByDate, FileType)
 			VALUES ('$TaskTitle','$Description','$TaskType','$SampleFile','$NumOfPages','$NumOfWords','$Completion','$ClaimBy','$FileType')";
+	if($tags >= 1)
+		$sql = "INSERT INTO Tags(Tag_text)
+				VALUES ('£Tags1')";
+	if($tags >= 2)
+		$sql = "INSERT INTO Tags(Tag_text)
+				VALUES ('£Tags2')";
+	if($tags >= 3)
+		$sql = "INSERT INTO Tags(Tag_text)
+				VALUES ('£Tags3')";
+	if($tags == 4)
+		$sql = "INSERT INTO Tags(Tag_text)
+				VALUES ('£Tags4')";
+				
 	$result = mysqli_query($connect,$sql);
 ?>
