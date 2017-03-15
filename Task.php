4450e9f3b141661053d7343d25ff6a340fd2bd44
ChunkyMitts <15160009@studentmail.ul.ie>
2017-03-15 19:54:25 +0000

    still trying to perfect it

--- Task.php
+++ Task.php
@@ -5,33 +5,54 @@
 	$TaskType =		$_POST['TaskType'];
 	$NumOfWords = 	$_POST['NumOfWords'];
 	$tags = 		$_POST['Tags'];
-	if($tags >= 1)
-		$Tags1 = 		$_POST['Tags1'];
-	if($tags >= 2)
-		$Tags2 = 		$_POST['Tags2'];
-	if($tags >= 3)
-		$Tags3 = 		$_POST['Tags3'];
-	if($tags == 4)
-		$Tags4 = 		$_POST['Tags4'];
 	$Description = 	$_POST['Description'];
 	$ClaimBy = 		$_POST['ClaimBy'];
 	$Completion =	$_POST['Completion'];
 	$FileType = 	$_POST['FileType'];
 	$SampleFile = 	$_POST['SampleFile'];
-	$sql = "INSERT INTO tasks(Title, Text_description, Task_type, Attached_Files, NoOfPages, NoOfWords, Deadline, ClaimByDate, FileType)
-			VALUES ('$TaskTitle','$Description','$TaskType','$SampleFile','$NumOfPages','$NumOfWords','$Completion','$ClaimBy','$FileType')";
-	if($tags >= 1)
-		$sql = "INSERT INTO Tags(Tag_text)
-				VALUES ('£Tags1')";
-	if($tags >= 2)
-		$sql = "INSERT INTO Tags(Tag_text)
-				VALUES ('£Tags2')";
-	if($tags >= 3)
-		$sql = "INSERT INTO Tags(Tag_text)
-				VALUES ('£Tags3')";
-	if($tags == 4)
-		$sql = "INSERT INTO Tags(Tag_text)
-				VALUES ('£Tags4')";
-				
+	$id_1 = NULL;
+	$id_2 = NULL;
+	$id_3 = NULL;
+	$id_4 = NULL;
+	if($tags >= 1){
+		$Tags1 = 		$_POST['Tags1'];
+		$sql = "INSERT INTO tags(text)
+				VALUES ('$Tags1')";
+		$result = mysqli_query($connect,$sql);
+		$sql = "Select tag_id from tags
+				where $Tags1 == text";
+		$id_1 = mysqli_query($connect,$sql);
+	}
+	if($tags >= 2){
+		$Tags2 = 		$_POST['Tags2'];
+		$sql = "INSERT INTO tags(text)
+				VALUES ('$Tags2')";
+		$result = mysqli_query($connect,$sql);
+		$sql = "Select tag_id from tags
+				where $Tags2 == text";
+		$id_2 = mysqli_query($connect,$sql);
+	}
+	if($tags >= 3){
+		$Tags3 = 		$_POST['Tags3'];
+		$sql = "INSERT INTO tags(text)
+				VALUES ('$Tags3')";
+		$result = mysqli_query($connect,$sql);
+		$sql = "Select tag_id from tags
+				where $Tags3 == text";
+		$id_3 = mysqli_query($connect,$sql);
+	}
+	if($tags == 4){
+		$Tags4 = 		$_POST['Tags4'];
+		$sql = "INSERT INTO tags(text)
+				VALUES ('$Tags4')";
+		$result = mysqli_query($connect,$sql);
+		$sql = "Select tag_id from tags
+				where $Tags4 == text";
+		$id_4 = mysqli_query($connect,$sql);
+	}
+
+	$sql = "INSERT INTO tasks(user_id, title, text_description, task_type, Attached_files, no_of_pages, no_of_words, Deadline, claim_by_date, file_type, tag_1, tag_2, tag_3, tag_4)
+			VALUES ('1', '$TaskTitle','$Description','$TaskType','$SampleFile','$NumOfPages','$NumOfWords','$Completion','$ClaimBy','$FileType', '$id_1','$id_2','$id_3','$id_4')";			
 	$result = mysqli_query($connect,$sql);
+	header("location: CreateTask.html");
 ?>
