abb137a543525f1a5abc7e71c2b014190e6a697a
willoleary6 <15155528@studentmail.ul.ie>
2017-03-20 14:06:07 +0000

    just adding an extra button to the task stream

--- userProfile.php
+++ userProfile.php
@@ -58,6 +58,7 @@
 							<nav id="nav">
 								<ul>
 									<li><a class="icon fa-cog" href="editProfile.html"><span>Edit profile</span></a></li>
+									<li><a class="icon fa-cog" href="task stream.php"><span>Task stream</span></a></li>
 									<li><a class="icon fa-retweet" href="index.php"><span>Log out</span></a></li>
 									<li><a class="icon fa-sitemap" href="CreateTask.php"><span>Create task</span></a></li>
 								</ul>
