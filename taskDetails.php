eb1d23de3b34c4b602b75e78ed94e74b647a13f9
ChunkyMitts <15160009@studentmail.ul.ie>
2017-03-28 17:14:21 +0100

    task stream linked to details
    Task stream has been linked to details so when you choose a task you
    will be brought to a personalised page of that task still barebones at
    the moment

--- taskDetails.php
+++ taskDetails.php
@@ -5,20 +5,15 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 <?php
-include 'dbh.php';
-$sql = "SELECT claim_id FROM `taskStatus` WHERE status_id = '1'";
-$result = mysqli_query($connect,$sql);
-$index = 0;
-if(mysqli_num_rows($result) > 0 ){
-while($array = mysqli_fetch_array($result)){
-   $claims[$index] = $array['claim_id'];
-   $index++;
- }
-}
+	include 'dbh.php';
+	$id = $_POST['text'];
+	$sql = "SELECT * from tasks WHERE task_id = $id";
+	$result = mysqli_query($connect,$sql);
+	$row = mysqli_fetch_assoc($result);
 ?>
 <html>
 	<head>
-		<title>Left Sidebar - Strongly Typed by HTML5 UP</title>
+		<title><?php print($row['title']);?></title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
@@ -33,7 +28,7 @@ while($array = mysqli_fetch_array($result)){
 					<div id="header" class="container">
 
 						<!-- Logo -->
-							<h1 id="logo"><a href="index.html">Task Stream</a></h1>
+							<h1 id="logo"><a href="index.html"><?php print($row['title'])?></a></h1>
 							
 
 						<!-- Nav -->
@@ -63,7 +58,7 @@ while($array = mysqli_fetch_array($result)){
 												</li>
 												<li>
 												<form action="CreateTask.php">
-							                      <input type="submit" value="Create task">
+							                      <input type="submit" value="ClaimTask">
 							                      </form>
 												</li>
 												<li>
@@ -81,34 +76,23 @@ while($array = mysqli_fetch_array($result)){
 
 							<!-- Content -->
 								<div id="content" class="8u 12u(mobile) important(mobile)">
-                                                 <?php
-												  for($i = 0; $i < sizeof($claims); $i++){
-												  $sql = "SELECT task_id FROM `task_claims` WHERE claim_id = '$claims[$i]'";  
-												  $result1 = mysqli_query($connect,$sql);
-												  $task = mysqli_fetch_array($result1);
-												  $sql = "SELECT * from tasks WHERE task_id = $task[0]";
-												  $result = mysqli_query($connect,$sql);
-												  $row = mysqli_fetch_assoc($result);
-												 ?>
+                                                 
 													<section>
-                                                    <br>
-													<h2> task: <?php print($row['title']);
-                                                      ?> </h2>
-													<h3> Type: <?php print($row['task_type'])?>
+													<h3>
+													Type: <?php print($row['task_type'])?>
 													<br>
-													Claim by: <?php print($row['claim_by_date'])?> </h3>
-													<p>Basic info about task: <?php print($row['text_description'])?> 
+													Claim by: <?php print($row['claim_by_date'])?> 
+													<br>
+													DeadLine Date: <?php print($row['Deadline'])?></h3>
+													<p>Description: <?php print($row['text_description'])?> 
 													<br>
 													Number of pages: <?php print($row['no_of_pages'])?>
 													<br>
-													Number of words: <?php print($row['no_of_words'])?><p>
-													<form action="taskDetails.php" method ="post">
-													<input type = "hidden" name ="text" value = "<?php print($row['task_id'])?>">
-							                        <input type="submit" value="View details">
-							                        </form>
-													</section>
+													Number of words: <?php print($row['no_of_words'])?>
+													<br>
+													File Type: <?php print($row['file_type'])?><p>
 
-												  <?php } ?>
+													</section>												  
 									
 								</div>
 
