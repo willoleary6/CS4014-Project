3d4a6edbff32197e91422b0f732073801468332a
piratebernard <bernardsteemers@BNi.local>
2017-04-25 18:54:35 +0100

    final
    commit

--- userProfile.php
+++ userProfile.php
@@ -1,5 +1,26 @@
 <!DOCTYPE HTML>
 <!--Code Written by bernard Steemers-->
+<script>
+/* taken readCookie from stack overflow
+http://stackoverflow.com/questions/10730362/get-cookie-by-name*/
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function isMod() {
+	var rep = readCookie('RepScore');
+	if(rep < 40) {
+	    var btn1 = document.getElementById('mod');
+        btn1.style.visibility = 'hidden';
+	}
+}
+</script>
 <html>
 	<head>
 		<title>[User Profile]</title>
@@ -10,14 +31,16 @@
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 	</head>
 	
-	<body class="no-sidebar">
+	<body class="no-sidebar" onload ="isMod()">
 		<div id="page-wrapper">
+		
+		<!-- Including database,cookies and assigning values -->
+		
             <?php
  				include 'dbh.php';
  				include 'cookieCheck.php';
  				$email = $_COOKIE['email'];
 	 			$password = $_COOKIE['password'];
-				$reputation_score = $_COOKIE['RepScore'];
  				$user_id = $_COOKIE['userID']; 
 				//getting the users name and reputation
 				$sql = "SELECT * FROM user_details Where email = '$email'";
@@ -26,7 +49,16 @@
  				{
   					$first_name = htmlspecialchars($row['first_name'],ENT_QUOTES);
  					$last_name = htmlspecialchars($row['last_name'],ENT_QUOTES);
+                    $reputation_score = $row['reputation_score'];
+					$student_staff_id = $row['student_staff_id'];
+					$subject_id = $row['subject_id'];
+					$sql = "SELECT subject_name FROM major_subjects where subject_id = '$subject_id'";
+					$result = mysqli_query($connect,$sql);
+					if($row = $result->fetch_assoc()){
+						$subject_name = $row['subject_name'];
+					}			 
   				}
+                
   		    ?>
   		    
             <!-- Header -->
@@ -37,11 +69,26 @@
 						<h1 id="logo"> <?php echo "<div style = 'margin:30px 0px;'> $first_name $last_name </div>"; ?> </h1><br>
 						<h2 id ="logo"> <?php echo "<div style = 'margin:30px 0px;'> Rep Score: $reputation_score </div>"; ?> </h2>
 						<p>Welcome, please view any tasks you have created or claimed here.</p>
-						
+					    <br>
+						<section>
+						    <h3>First Name: <?php echo "'$first_name'" ?>
+							<br>
+							Last Name: <?php echo "'$last_name'" ?>
+							<br>
+							Student/Staff ID: <?php echo "'$student_staff_id'" ?>
+							<br>
+							Email: <?php echo "'$email'" ?>
+							<br>
+							Field: <?php echo "'$subject_name'" ?>
+							<br><br>
+							<li><a class="icon fa-cog" href="deleteUser.php"><span>Delete Account</span></a></li>
+							<h3>
+						</section>	
                         <!-- Nav -->
 							<nav id="nav">
 								<ul>
 									<li><a class="icon fa-cog" href="editDetails.php"><span>Edit profile</span></a></li>
+									<li><a class="icon fa-cog" href="FAQs.html"><span>FAQ</span></a></li>
 									<li><a class="icon fa-cog" href="taskStream.php"><span>Task stream</span></a></li>
 									<li><a class="icon fa-retweet" href="logout.php"><span>Log out</span></a></li>
 									<li><a class="icon fa-sitemap" href="CreateTask.php"><span>Create task</span></a></li>
@@ -55,52 +102,18 @@
 				<div id="main-wrapper">
 					<div id="main" class="container">
 						<div id="content">
+							<button type="button" id = "mod" onclick="javascript:location.href='FlaggedTasks.php'">View Flagged Tasks</button>
 							<div class="row">
 							<!-- User Created Tasks -->
 								<div class="6u 12u(mobile)">
 									
-									<section>
-										<h2>User Details</h2>
-									<section>
 									
-									<?php 
-										
-										$sql = "SELECT * FROM user_details where user_id = '$user_id'";
-  										$result = mysqli_query($connect,$sql);
-  										if($row = $result->fetch_assoc())
-  										{
-  											$student_staff_id = $row['student_staff_id'];
-  											$subject_id = $row['subject_id'];
-  										}
-										
-										$sql = "SELECT subject_name FROM major_subjects where subject_id = '$subject_id'";
-  										$result = mysqli_query($connect,$sql);
-  										if($row = $result->fetch_assoc())
-  										{
-  											$subject_name = $row['subject_name'];
-  										}
-										
-									?>
 									
-									<section>
-										<h3>First Name: <?php echo "$first_name" ?>
-										<br>
-										Last Name: <?php echo "$last_name" ?>
-										<br>
-										Student/Staff ID: <?php echo "$student_staff_id" ?>
-										<br>
-										Email: <?php echo "$email" ?>
-										<br>
-										Field: <?php echo "$subject_name " ?>
-										<br><br>
-										<li><a class="icon fa-cog" href="deleteUser.php"><span>Delete Account</span></a></li>
-										<h3>
-									<section>
-										
 										
 									<section>
 										<h2>Your Created Tasks</h2>
 									</section>
+									
 									
 									<?php
 										// selecting all the information from the tasks the user has created
@@ -120,6 +133,9 @@
 										$Status = mysqli_fetch_assoc($CurStat);
 												 
 									?>
+									
+									<!-- Printing out task details -->
+									
 											<section>
 											<br>
 											<h2> task: <?php print(htmlspecialchars($row['title'], ENT_QUOTES));
@@ -152,16 +168,17 @@
 									</section>
 									
 								<?php
-									// selecting all the information from the tasks the user has created
 									$sql = "SELECT claim_id from taskStatus WHERE status_id = 2";
 									$result = mysqli_query($connect,$sql);
 									$i = 0;
-									While($claimIDS = $result->fetch_assoc())
-									{
-										$claim_ids[$i] = htmlspecialchars($claimIDS['claim_id'],ENT_QUOTES);
+									While($claimIDS = $result->fetch_assoc()) {
+										$claim_ids[$i] = $claimIDS['claim_id'];
 										$i++;
 									}
-								    for($b = 0; $b < sizeof($claim_ids);$b++)
+									if($claim_ids[0] = NULL) {
+										$claim_ids[0] = 0;
+									}
+									for($b = 0; $b < sizeof($claim_ids);$b++)
 								    {
 										$sql = "SELECT task_id from task_claims Where user_id = '$user_id' AND claim_id = '$claim_ids[$b]'";
 										$result = mysqli_query($connect,$sql);
